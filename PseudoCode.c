c0bb0cea64b43033b8b0ce7bc65459a983c8443c
NoFocusGaming <102929170+NoFocusGaming@users.noreply.github.com>
2022-04-03 16:27:15 -0700

    Update pseudocode.c

--- PseudoCode.c
+++ PseudoCode.c
@@ -3,3 +3,15 @@
 int monitorTemp();
 int monitorWaterLevel();
 int monitorHumidity();
+
+//different system states
+void disabled();
+  //yellow LED on
+  //use IRS to monitor start button
+
+void idle();
+  //call monitorTemp()
+    //transition to 'running' state when temp > thresh
+
+void error();
+void running();
