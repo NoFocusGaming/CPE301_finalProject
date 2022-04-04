1e5f199882e2261bbcf9a6b93e136f325fbb355b
NoFocusGaming <102929170+NoFocusGaming@users.noreply.github.com>
2022-04-03 17:21:39 -0700

    Adding VentChanges function

--- PseudoCode.c
+++ PseudoCode.c
@@ -35,11 +35,18 @@ int monitorTemp();
 int monitorWaterLevel();
 int monitorHumidity();
 
+void ventChanges(){
+ // read vent potentiometer level
+ // adjust motor accordingly
+}
+
 //different system states
 int disabled(){
   //yellow LED on
   //use IRS to monitor start button
-  while (/*not button pressed*/);
+  while (/*not button pressed*/){
+   ventChanges(); 
+  }
   return 1;
 }  
   
@@ -65,7 +72,9 @@ int error(){
  //turn red LED on  
  //display error message on LCD
   
- while( /*check for reset button to be pressed*/ );
+ while( /*check for reset button to be pressed*/ ){
+  ventChanges(); 
+ }
   
  //turn red LED off
   
@@ -81,7 +90,9 @@ int running(){
  //turn blue LED on 
  //turn fan motor on
   
- while( (monitorTemp() > thresh) && (monitorWaterLevel() > minWaterLvl));
+ while( (monitorTemp() > thresh) && (monitorWaterLevel() > minWaterLvl)){
+  ventChanges(); 
+ }  
   
  //turn blue LED off
  //turn fan motor off
